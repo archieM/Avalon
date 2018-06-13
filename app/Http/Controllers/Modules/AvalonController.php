f6b0011c5d1d6920d8999e97221118ede1b0c97b
archieM <archiemorre@gmail.com>
2018-06-13 18:01:55 +0800

    Avalon

---
+++ app/Http/Controllers/Modules/AvalonController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Modules;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AvalonController extends Controller
+{
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        echo "store";
+    }
+
+   
+}
